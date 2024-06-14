9628b2326b7a3e39d9523a24c555a65450bf8ffb
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2024-06-14 16:46:17 +0700

    feat(lib): updated zod

--- src/app/_lib/zod.ts
+++ src/app/_lib/zod.ts
@@ -1,23 +1,26 @@
 import { object, string } from "zod";
 
-export const signInSchema = object({
+const emailObject = object({
     email: string({ required_error: "Email is required" })
-        .min(1, "Email is required")
-        .email("Invalid email"),
-    password: string({ required_error: "Password is required" })
-        .min(1, "Password is required")
-        .min(8, "Password must be more than 8 characters")
-        .max(32, "Password must be less than 32 characters"),
+      .min(0, "Email is required")
+      .email("Invalid email address")
 });
 
-export const registerSchema = object({
-    email: string({ required_error: "Email is required" })
-        .min(3, "Email is required")
-        .email("Invalid email"),
+const passwordObject = object({
     password: string({ required_error: "Password is required" })
-        .min(1, "Password is required")
-        .min(8, "Password must be more than 8 characters")
-        .max(32, "Password must be less than 32 characters"),
-    name: string({ required_error: "Name is required" })
-        .min(3, "Name is required")
+      .min(0, "Password is required")
+      .min(8, "Password must be more than 8 characters")
+      .max(32, "Password must be less than 32 characters")
 });
+
+export const signInSchema = emailObject.merge(passwordObject);
+
+export const registerSchema = object({
+    name: string({ required_error: "Name is required" })
+      .min(3, "Name must be more than 3 characters")
+      .max(32, "Name must be less than 32 characters")
+})
+  .merge(emailObject)
+  .merge(passwordObject);
+
+export const resetSchema = emailObject;
