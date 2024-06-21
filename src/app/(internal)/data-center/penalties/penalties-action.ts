206b29af6574babc78ba5f440da27b4399dc13f9
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2024-06-21 18:12:51 +0700

    feat(penalties): updated action, db and zod

--- src/app/(internal)/data-center/penalties/penalties-action.ts
+++ src/app/(internal)/data-center/penalties/penalties-action.ts
@@ -1,28 +1,102 @@
-export async function createPenalty(description: string, amount: number, bookingId: number) {
-  return prisma.penalty.create({
-    data: {
-      description,
-      amount,
-      booking_id: bookingId,
-    },
+import { Penalty, Prisma } from "@prisma/client";
+import { OmitIDTypeAndTimestamp } from "@/app/_db/db";
+import { createPenalty, deletePenalty, updatePenaltyByID } from "@/app/_db/penalty";
+import { number, object } from "zod";
+import {penaltySchemaWithID, penaltySchema} from "@/app/_lib/zod/penalties/zod";
+
+export type PenaltyActionsType<T = OmitIDTypeAndTimestamp<Penalty>> = {
+  success?: string | Penalty;
+  failure?: string;
+  errors?: Partial<Penalty>;
+};
+
+export async function createPenaltyAction(prevState: PenaltyActionsType, formData: FormData): Promise<PenaltyActionsType> {
+  const parsedData = penaltySchema.safeParse({
+    amount: parseFloat(formData.get("amount") as string),
+    description: formData.get("description"),
+    booking_id: formData.get("booking_id") ? parseInt(formData.get("booking_id") as string) : undefined,
   });
+
+  if (!parsedData.success) {
+    return {
+      errors: Object.fromEntries(parsedData.error.errors.entries()),
+    };
+  }
+
+  try {
+    let res = await createPenalty({
+      ...parsedData.data,
+      booking_id: parsedData.data.booking_id ?? null,
+      amount: new Prisma.Decimal(parsedData.data.amount)
+    });
+
+    return {
+      success: res,
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      failure: "Error creating penalty",
+    };
+  }
 }
 
-export async function getPenaltiesByBooking(bookingId: number) {
-  return prisma.penalty.findMany({
-    where: {booking_id: bookingId},
+export async function updatePenaltyAction(prevState: PenaltyActionsType, formData: FormData): Promise<PenaltyActionsType> {
+  const parsedData = penaltySchemaWithID.safeParse({
+    id: parseInt(formData.get("id") as string),
+    amount: parseFloat(formData.get("amount") as string),
+    description: formData.get("description"),
+    booking_id: formData.get("booking_id") ? parseInt(formData.get("booking_id") as string) : undefined,
   });
+
+  if (!parsedData.success) {
+    return {
+      errors: Object.fromEntries(parsedData.error.errors.entries()),
+    };
+  }
+
+  try {
+    let res = await updatePenaltyByID(parsedData.data.id, {
+      ...parsedData.data,
+      booking_id: parsedData.data.booking_id ?? null,
+      amount: new Prisma.Decimal(parsedData.data.amount)
+    });
+
+    return {
+      success: res,
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      failure: "Error updating penalty",
+    };
+  }
 }
 
-export async function updatePenalty(id: number, description: string, amount: number) {
-  return prisma.penalty.update({
-    where: {id},
-    data: {description, amount},
+export async function deletePenaltyAction(prevState: PenaltyActionsType<Pick<Penalty, "id">>, formData: FormData): Promise<PenaltyActionsType<Pick<Penalty, "id">>> {
+  const parsedData = object({ id: number().positive() }).safeParse({
+    id: parseInt(formData.get("id") as string),
   });
-}
 
-export async function deletePenalty(id: number) {
-  return prisma.penalty.delete({
-    where: {id},
-  });
+  if (!parsedData.success) {
+    return {
+      errors: Object.fromEntries(parsedData.error.errors.entries()),
+    };
+  }
+
+  try {
+    let res = await deletePenalty(parsedData.data.id);
+
+    return {
+      success: res,
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      failure: "Error deleting penalty",
+    };
+  }
 }
--- src/app/_lib/zod/penalties/zod.ts
+++ src/app/_lib/zod/penalties/zod.ts
@@ -1,6 +1,11 @@
 import {number, object, string} from "zod";
 
 export const penaltySchema = object({
-  description: string().min(1, "Description is required"),
   amount: number().positive("Amount must be a positive number"),
+  description: string().min(1, "Description is required"),
+  booking_id: number().min(1, "Booking ID is required"),
+});
+
+export const penaltySchemaWithID = penaltySchema.extend({
+  id: number().positive("ID must be a positive number"),
 });
