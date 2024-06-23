393c5fc18272bc17e3d6ac6c0f269d674857b43f
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2024-06-23 17:19:35 +0700

    feat(dashboard): updated page

--- src/app/(internal)/dashboard/page.tsx
+++ src/app/(internal)/dashboard/page.tsx
@@ -1,5 +1,20 @@
-export default function DashboardPage() {
-    return (
-        <span>dashboard page</span>
-    );
+import styles from "./dashboard.module.css";
+import Overview from "@/app/(internal)/dashboard/_components/overview";
+import {DashboardContextProps} from "@/app/_context/DashboardContext";
+
+export default function DashboardPage(props?: DashboardContextProps) {
+
+
+  return (
+    <>
+      <div className={styles.dashboardContainer}>
+        <div className={styles.headerContainer}>
+          <h1 className={styles.header}>Dashboard</h1>
+        </div>
+        <div className={styles.content}>
+          <Overview locationID={props?.locationID}/>
+        </div>
+      </div>
+    </>
+  );
 }
