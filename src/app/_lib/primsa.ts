35eb7a7d11f73c80f65a5ef3d3f59e13a28bbd93
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2024-06-24 15:05:27 +0700

    feat(prisma): added query logging comment

--- src/app/_lib/primsa.ts
+++ src/app/_lib/primsa.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import {PrismaClient} from '@prisma/client';
 
 declare global {
   var prisma: PrismaClient;
@@ -10,9 +10,22 @@ if (process.env.NODE_ENV === 'production') {
   prismaClient = new PrismaClient();
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient();
+    global.prisma = new PrismaClient({
+      log: [
+        {
+          emit: "event",
+          level: "query",
+        },
+      ],
+    });
   }
   prismaClient = global.prisma;
+
+  // // @ts-ignore
+  // prisma.$on("query", async (e) => {
+  //   // @ts-ignore
+  //   console.log(`${e.query} ${e.params}`);
+  // });
 }
 
 export default prismaClient;
