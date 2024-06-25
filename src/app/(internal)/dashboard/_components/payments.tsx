1ee3de9857ab8425fc9cb21ec995eecb768a6643
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2024-06-26 02:19:51 +0700

    feat(components): moved tantable to its own component

--- src/app/(internal)/dashboard/_components/payments.tsx
+++ src/app/(internal)/dashboard/_components/payments.tsx
@@ -6,10 +6,11 @@ import {useQuery} from "@tanstack/react-query";
 import {getPaymentData, getPaymentStatuses} from "@/app/_db/dashboard";
 
 import styles from "./styles/payments.module.css";
-import {Button, Chip, Typography} from "@material-tailwind/react";
-import {createColumnHelper, flexRender, getCoreRowModel, useReactTable} from "@tanstack/react-table";
+import {Button, Chip} from "@material-tailwind/react";
+import {createColumnHelper, getCoreRowModel, useReactTable} from "@tanstack/react-table";
 import {colors} from "@material-tailwind/react/types/generic";
 import {AiOutlineLoading} from "react-icons/ai";
+import TanTable from "@/app/_components/tanTable";
 
 const fallbackData: never[] = [];
 
@@ -102,48 +103,7 @@ export default function Payments() {
         }
         {
           paymentsIsSuccess &&
-            <table className="w-full min-w-max table-auto text-left rounded-t-lg">
-                <thead className={"sticky top-0 z-10"}>
-                {tanTable.getHeaderGroups().map(headerGroup => (
-                  <tr key={headerGroup.id}>
-                    {headerGroup.headers.map(header => (
-                      <th key={header.id} className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-normal leading-none opacity-70"
-                        >
-                          {header.isPlaceholder
-                            ? null
-                            : flexRender(
-                              header.column.columnDef.header,
-                              header.getContext()
-                            )}
-                        </Typography>
-
-                      </th>
-                    ))}
-                  </tr>
-                ))}
-                </thead>
-                <tbody>
-                {tanTable.getRowModel().rows.map((row, index) => {
-                  const isLast = index === tanTable.getRowModel().rows.length - 1;
-                  const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
-
-                  return (<tr key={row.id}>
-                      {row.getVisibleCells().map(cell => (
-                        <td key={cell.id} className={classes}>
-                          <div className={"text-sm text-gray-700 font-normal"}>
-                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                          </div>
-                        </td>
-                      ))}
-                    </tr>
-                  );
-                })}
-                </tbody>
-            </table>
+            <TanTable tanTable={tanTable}/>
         }
       </div>
     </div>
