3af2ca7d27b6c1d7fa9a0330cb6beac8f29412d6
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2024-06-25 15:08:11 +0700

    feat(zod): updated expense to reflect changes on expense table

--- src/app/_lib/zod/expenses/zod.ts
+++ src/app/_lib/zod/expenses/zod.ts
@@ -1,10 +1,11 @@
-import { number, object, string, typeToFlattenedError} from "zod";
+import {number, object, string} from "zod";
 
 export const expenseObject = object({
   amount: number().positive("Amount must be a positive number"),
   description: string().min(1, "Description is required"),
   date: string().min(1, "Date is required"),
-  category_id: number().optional(),
+  category_id: number().nullable(),
+  location_id: number().min(0, "Location ID is required"),
 });
 
 export const expenseObjectWithID = expenseObject.extend({
