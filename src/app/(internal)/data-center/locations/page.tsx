b2e6d74db5ff22cdfb16f85d87a37cc7315fad9e
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2024-07-03 10:30:32 +0700

    feat(locations): added page

--- src/app/(internal)/data-center/locations/page.tsx
+++ src/app/(internal)/data-center/locations/page.tsx
@@ -1,3 +1,13 @@
-export default function LocationsPage() {
-  return <></>;
+import {getAllLocations} from "@/app/_db/location";
+import {LocationTable} from "@/app/(internal)/data-center/locations/components/table";
+import styles from "./location.module.css";
+
+export default async function LocationsPage() {
+  const locations = await getAllLocations();
+
+  return (
+    <div className={styles.locationsPageContainer}>
+      <LocationTable locations={locations}/>
+    </div>
+  );
 }
