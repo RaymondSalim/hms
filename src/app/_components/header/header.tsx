06b1b420171b9c1bb6fbbcec7e15ac2817f98374
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2024-07-04 16:03:11 +0700

    feat(header): added reset context on path change

--- src/app/_components/header/header.tsx
+++ src/app/_components/header/header.tsx
@@ -1,15 +1,23 @@
 "use client";
 
-import React, {PropsWithChildren, useContext} from "react";
+import React, {PropsWithChildren, useContext, useEffect} from "react";
 import {HeaderContext} from "@/app/_context/HeaderContext";
 import {Breadcrumbs} from "@material-tailwind/react";
 import LocationPicker from "@/app/_components/header/locationPicker";
 import styles from "./header.module.css";
 import {IoHome} from "react-icons/io5";
 import Link from "next/link";
+import {usePathname} from "next/navigation";
 
 export default function Header({children}: PropsWithChildren) {
   const headerContext = useContext(HeaderContext);
+  const pathName = usePathname();
+
+  useEffect(() => {
+    headerContext.setShowLocationPicker(true);
+    headerContext.setShow(true);
+  }, [pathName]);
+
   return (
     <div className={"p-8 pb-0"}>
       <div className={styles.breadcrumbsLocationContainer}>
