39a0392f38255294b5c780cbbf2ff359f0f0eb57
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2024-07-10 11:06:09 +0700

    feat(guests): moved pages and layouts

--- src/app/(internal)/residents/guests/page.tsx
+++ src/app/(internal)/residents/guests/page.tsx
@@ -1,11 +1,44 @@
-import React from "react";
+"use client";
+
+import React, {useContext, useEffect} from "react";
 import {getGuests} from "@/app/_db/guest";
 import GuestsContent from "@/app/(internal)/residents/guests/content";
+import {HeaderContext} from "@/app/_context/HeaderContext";
+import Link from "next/link";
+import {useQuery} from "@tanstack/react-query";
+import {AiOutlineLoading} from "react-icons/ai";
 
-export default async function UsersPage() {
-  const guests = await getGuests();
+interface GuestsPageProps {
+  locationID?: number
+}
+
+export default function GuestsPage(props: GuestsPageProps) {
+  const headerContext = useContext(HeaderContext);
+
+  useEffect(() => {
+    headerContext.setTitle("Guests");
+    headerContext.setShowLocationPicker(true);
+    headerContext.setPaths([
+      <Link key={"residents"} href={"/residents"}>Residents</Link>,
+      <Link key={"guests"} href={"/guests"}>Guests</Link>
+    ]);
+  }, []);
+
+  const {data: guests, isLoading, isSuccess} = useQuery({
+    queryKey: ['guests', headerContext.locationID],
+    queryFn: () => getGuests(undefined, headerContext.locationID),
+  });
 
   return (
-    <GuestsContent guests={guests}/>
+    <>
+      {
+        isLoading &&
+          <span className={"mx-auto h-8 w-8"}><AiOutlineLoading className="animate-spin"/></span>
+      }
+      {
+        isSuccess &&
+          <GuestsContent guests={guests}/>
+      }
+    </>
   );
 }
