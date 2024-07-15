1109227b91050615ccddf63bc81951e144417a50
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2024-07-15 16:19:17 +0700

    feat(rooms): added durations page

---
+++ src/app/(internal)/rooms/durations/content.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import {createColumnHelper} from "@tanstack/react-table";
+import React from "react";
+import {formatToDateTime} from "@/app/_lib/util";
+import {TableContent} from "@/app/_components/pageContent/TableContent";
+import {Duration} from "@prisma/client";
+import {DurationForm} from "@/app/(internal)/rooms/durations/form";
+import {deleteDurationAction, upsertDurationAction} from "@/app/(internal)/rooms/durations/duration-actions";
+
+
+interface ContentProps<T> {
+  contents: T[]
+}
+
+export default function DurationsContent({contents}: ContentProps<Duration>) {
+  const columnHelper = createColumnHelper<Duration>();
+  const columns = [
+    columnHelper.accessor(row => row.id, {
+      header: "ID",
+      size: 20
+    }),
+    columnHelper.accessor(row => row.duration, {
+      header: "Duration"
+    }),
+    columnHelper.accessor(row => row.day_count, {
+      header: "Day Count",
+      cell: props => props.cell.getValue() ?? "-",
+      enableGlobalFilter: false
+    }),
+    columnHelper.accessor(row => row.month_count, {
+      header: "Month Count",
+      cell: props => props.cell.getValue() ?? "-",
+      enableGlobalFilter: false
+    }),
+    columnHelper.accessor(row => row.createdAt, {
+      header: "Created At",
+      cell: props => formatToDateTime(props.cell.getValue()),
+      enableGlobalFilter: false,
+    }),
+  ];
+
+  return (
+    <div className={"p-8"}>
+      <TableContent<typeof contents[0]>
+        name={"Durations"}
+        initialContents={contents}
+        columns={columns}
+        form={
+          // @ts-ignore
+          <DurationForm/>
+        }
+        searchPlaceholder={"Search by duration"}
+        upsert={{
+          mutationFn: upsertDurationAction,
+        }}
+
+        delete={{
+          // @ts-ignore
+          mutationFn: deleteDurationAction,
+        }}
+      />
+    </div>
+  );
+}
---
+++ src/app/(internal)/rooms/durations/duration-actions.ts
@@ -0,0 +1,86 @@
+"use server";
+
+import {Duration} from "@prisma/client";
+import {PrismaClientKnownRequestError, PrismaClientUnknownRequestError} from "@prisma/client/runtime/library";
+import {GenericActionsType} from "@/app/_lib/actions";
+import {number, object, ZodIssueCode} from "zod";
+import {durationSchemaWithOptionalID} from "@/app/_lib/zod/duration/zod";
+import {createDuration, deleteDuration, updateDurationByID} from "@/app/_db/duration";
+import {IntersectionToUnion} from "@/app/_lib/util";
+
+export async function upsertDurationAction(roomData: Partial<Duration>): Promise<GenericActionsType<Duration>> {
+  const {success, data, error} = durationSchemaWithOptionalID.safeParse(roomData);
+
+  if (!success) {
+    if (error?.issues?.[0].code == ZodIssueCode.custom) {
+      return {
+        failure: error.errors[0].message
+      };
+    }
+    return {
+      errors: error?.formErrors
+    };
+  }
+
+  let dataType: IntersectionToUnion<typeof data> = data;
+
+  try {
+    let res;
+    // Update
+    if (data?.id) {
+      // @ts-ignore
+      res = await updateDurationByID(data.id, dataType);
+    } else {
+      // @ts-ignore
+      res = await createDuration(data);
+    }
+
+    return {
+      success: res
+    };
+
+  } catch (error) {
+    if (error instanceof PrismaClientKnownRequestError) {
+      console.error("[upsertDurationAction]", error.code, error.message);
+      switch (error.code) {
+        case "P2002":
+          return {failure: "Duration Name is taken."};
+      }
+    } else if (error instanceof PrismaClientUnknownRequestError) {
+      console.error("[upsertDurationAction]", error.message);
+    }
+
+    return {failure: "Request unsuccessful"};
+  }
+}
+
+export async function deleteDurationAction(id: string): Promise<GenericActionsType<Duration>> {
+  const parsedData = object({id: number().positive()}).safeParse({
+    id: id,
+  });
+
+  if (!parsedData.success) {
+    return {
+      errors: parsedData.error.formErrors
+    };
+  }
+
+  try {
+    let res = await deleteDuration(parsedData.data.id);
+    return {
+      success: res,
+    };
+  } catch (error) {
+    if (error instanceof PrismaClientKnownRequestError) {
+      console.error("[deleteDurationAction]", error.code, error.message);
+      switch (error.code) {
+        case "P2003":
+          return {failure: "There are rooms with this duration.\nPlease remove or change the room duration first"};
+      }
+    } else if (error instanceof PrismaClientUnknownRequestError) {
+      console.error("[deleteDurationAction]", error.message);
+    }
+
+    return {failure: "Request unsuccessful"};
+  }
+}
