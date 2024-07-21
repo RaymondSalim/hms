914d4d95ed24bbf745a45a644c53219ad7839cca
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2024-07-21 12:51:16 +0700

    feat(rooms): added pricing details

--- src/app/(internal)/rooms/all-rooms/content.tsx
+++ src/app/(internal)/rooms/all-rooms/content.tsx
@@ -1,21 +1,35 @@
 "use client";
 
 import {createColumnHelper} from "@tanstack/react-table";
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import {formatToDateTime} from "@/app/_lib/util";
 import {TableContent} from "@/app/_components/pageContent/TableContent";
 import {RoomsWithTypeAndLocation} from "@/app/_db/room";
 import {RoomForm} from "@/app/(internal)/rooms/all-rooms/form";
 import {deleteRoomAction, upsertRoomAction} from "@/app/(internal)/rooms/all-rooms/room-actions";
 import {HeaderContext} from "@/app/_context/HeaderContext";
+import {Button, Dialog, Typography} from "@material-tailwind/react";
+import {useQuery} from "@tanstack/react-query";
+import {getDurations} from "@/app/_db/duration";
+import Link from "next/link";
 
 
 export interface RoomsContentProps {
   rooms: RoomsWithTypeAndLocation[]
 }
 
+const detailsHeader = ["Duration", "Price"];
+
 export default function RoomsContent({rooms}: RoomsContentProps) {
   const headerContext = useContext(HeaderContext);
+  const [activeData, setActiveData] = useState<RoomsWithTypeAndLocation | undefined>(undefined);
+  const [showDialog, setShowDialog] = useState(false);
+
+  const {data: durationsData, isSuccess, isLoading} = useQuery({
+    queryKey: ['rooms.durations'],
+    queryFn: () => getDurations(),
+  });
+
 
   const columnHelper = createColumnHelper<RoomsWithTypeAndLocation>();
   const columns = [
@@ -32,6 +46,15 @@ export default function RoomsContent({rooms}: RoomsContentProps) {
     columnHelper.accessor(row => row.roomstatuses?.status, {
       header: "Status",
     }),
+    columnHelper.display({
+      header: "Pricing",
+      cell: props =>
+        <Link className={"text-blue-400"} type="button" href="" onClick={() => {
+          setActiveData(props.row.original);
+          setShowDialog(true);
+        }}>Pricing Details</Link>
+    }),
+
     columnHelper.accessor(row => row.createdAt, {
       header: "Created At",
       cell: props => formatToDateTime(props.cell.getValue())
@@ -66,6 +89,79 @@ export default function RoomsContent({rooms}: RoomsContentProps) {
           // @ts-ignore
           mutationFn: deleteRoomAction,
         }}
+        customDialog={
+          <Dialog
+            open={showDialog}
+            size={"md"}
+            handler={() => setShowDialog(prev => !prev)}
+            className={"p-8"}
+          >
+            <Typography variant="h5" color="black" className="mb-4">Pricing Details</Typography>
+            {
+              activeData?.roomtypes?.roomtypedurations && isSuccess &&
+                <table className="w-full min-w-max table-auto text-left">
+                    <thead>
+                    <tr>
+                      {detailsHeader.map((el) => (
+                        <th
+                          key={el}
+                          className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
+                        >
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal leading-none opacity-70"
+                          >
+                            {el}
+                          </Typography>
+                        </th>
+                      ))}
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {durationsData.map((d, index, arr) => {
+                      const isLast = index === arr.length - 1;
+                      const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
+
+                      const mapping = activeData.roomtypes?.roomtypedurations.find(e => e.duration_id == d.id);
+
+                      return (
+                        <tr key={d.id}>
+                          <td className={classes}>
+                            <Typography
+                              variant="small"
+                              color="blue-gray"
+                              className="font-normal"
+                            >
+                              {d.duration}
+                            </Typography>
+                          </td>
+                          <td className={classes}>
+                            <Typography
+                              variant="small"
+                              color={mapping ? "blue-gray" : "red"}
+                              className="font-normal"
+                            >
+                              {
+                                mapping ?
+                                  `IDR ${mapping.suggested_price}` :
+                                  'Not Set'
+                              }
+                            </Typography>
+                          </td>
+                        </tr>
+                      );
+                    })}
+                    </tbody>
+                </table>
+            }
+            <div className={"flex gap-x-4 justify-end"}>
+              <Button onClick={() => setShowDialog(false)} variant={"outlined"} className="mt-6">
+                Close
+              </Button>
+            </div>
+          </Dialog>
+        }
       />
     </div>
   );
