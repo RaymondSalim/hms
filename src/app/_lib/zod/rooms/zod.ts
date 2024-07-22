6e4b2114f1f560d87791c38f88a2af509d8fab68
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2024-07-22 14:18:22 +0700

    feat(zod): updated schema for room

--- src/app/_lib/zod/rooms/zod.ts
+++ src/app/_lib/zod/rooms/zod.ts
@@ -1,12 +1,55 @@
-import {number, object, string} from "zod";
+import {Prisma} from "@prisma/client";
+import {any, array, number, object, preprocess, record, string, z, ZodIssueCode} from "zod";
 
 export const roomSchema = object({
-    room_number: string({required_error: "Room Number is required"}),
-    room_type_id: number({required_error: "Room Type ID is required"}),
-    status_id: number({required_error: "Status ID is required"}),
-    location_id: number({required_error: "Location ID is required"}),
+  room_number: string({required_error: "Room Number is required"}),
+  roomstatuses: object({
+    id: number({required_error: "Status ID is required"})
+  }),
+  location_id: number({required_error: "Location ID is required"}),
 });
 
 export const roomSchemaWithOptionalID = roomSchema.extend({
-    id: number().positive().optional()
+  id: number().positive().optional()
 });
+
+export const roomWithType = roomSchemaWithOptionalID.extend({
+  roomtypes: object({
+    id: number({required_error: "Room Type ID is required"}),
+    roomtypedurations: array(
+      object({
+        id: number().min(1, "ID should be positive").optional(),
+        room_type_id: number({required_error: "Room Type ID is required"}),
+        duration_id: number().min(1, "Duration ID should be greater than zero").optional(),
+        durations: record(string(), any()).optional(),
+        suggested_price: preprocess(
+          (val) => {
+            if (typeof val == "string") {
+              return Number(val);
+            }
+
+            return val;
+          },
+          number({required_error: "Suggested Price is required"})
+            .min(1, "Suggested Price should be greater than 0")
+        ),
+      })
+    )
+  })
+})
+  .superRefine((input, ctx) => {
+    input.roomtypes.roomtypedurations.forEach(rtd => {
+      if (rtd.duration_id == undefined && !rtd.durations) {
+        ctx.addIssue({
+          code: ZodIssueCode.custom,
+          message: "Either Duration ID or duration object should be set",
+          fatal: true,
+        });
+
+        return z.NEVER;
+      }
+
+      // @ts-ignore
+      rtd.suggested_price = new Prisma.Decimal(rtd.suggested_price);
+    });
+  });
