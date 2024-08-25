c5ac0011f10a186d9b5f5596e28a0f26fc6cd04a
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2024-08-26 01:55:24 +0700

    feat(zod): updated payment schema

--- src/app/_lib/zod/payment/zod.ts
+++ src/app/_lib/zod/payment/zod.ts
@@ -1,23 +1,15 @@
-import {Prisma} from "@prisma/client";
-import {date, number, object, string} from "zod";
+import {date, number, object, string, z} from "zod";
 
 export const paymentSchema = object({
   id: number().min(1, "ID should be greater than 0").optional(),
   booking_id: number().min(1, "Booking ID is required"),
-  amount: number().positive("Amount must be a positive number"),
+  amount: z.coerce.number().positive("Amount must be a positive number"),
   payment_date: date({required_error: "Payment date is required"}),
   // TODO! Payment Proof
-  payment_proof: string().optional(),
+  payment_proof_file: object({
+    fileName: string(),
+    fileType: string(),
+    b64File: string().base64()
+  }).optional(),
   status_id: number({required_error: "Status ID is required"}).min(1, "Status ID should be greater than zero"),
-
-  numeric_amount: number().nullish()
-})
-  .superRefine(arg => {
-    if (arg.amount) {
-      arg.numeric_amount = arg.amount;
-      // @ts-expect-error
-      arg.amount = new Prisma.Decimal(arg.amount);
-    }
-
-    return arg;
-  });
+});
