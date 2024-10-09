6aaaaeb42fdced799894acd1633604dce19b1f55
Raymond Salim <rsalim@domaintools.com>
2024-10-09 15:06:11 +0800

    feat(db): improve payment actions

--- src/app/_db/payment.ts
+++ src/app/_db/payment.ts
@@ -5,89 +5,103 @@ import prisma from "@/app/_lib/primsa";
 import {OmitIDTypeAndTimestamp} from "@/app/_db/db";
 
 const includeAll = {
-  bookings: {
-    include: {
-      rooms: true,
-      tenants: true
-    }
-  },
-  paymentstatuses: true
+    bookings: {
+        include: {
+            rooms: true,
+            tenants: true
+        }
+    },
+    paymentstatuses: true
 };
 
 const paymentIncludeAll = Prisma.validator<Prisma.PaymentDefaultArgs>()({
-  include: includeAll
+    include: includeAll
 });
 
 export type PaymentIncludeAll = Prisma.PaymentGetPayload<typeof paymentIncludeAll> & {
-  bookings: {
-    custom_id: string
-  }
+    bookings: {
+        custom_id: string
+    }
 };
 
+const mapCustomID = (nb: Prisma.PaymentGetPayload<typeof paymentIncludeAll>[]) => nb.map(b => ({
+    ...b,
+    bookings: {
+        ...b.bookings,
+        custom_id: `#-${b.bookings.id}`
+    }
+}))
+
+
 export async function getAllPayments(id?: number, locationID?: number, limit?: number, offset?: number) {
-  return prisma.payment.findMany({
-    where: {
-      id: id,
-      bookings: {
-        rooms: {
-          location_id: locationID,
-        }
-      }
-    },
-    include: includeAll,
-    take: limit,
-    skip: offset,
-  })
-    .then(nb => nb.map(b => ({
-        ...b,
-        bookings: {
-          ...b.bookings,
-          custom_id: `#-${b.bookings.id}`
-        }
-      }))
-    );
+    return prisma.payment.findMany({
+        where: {
+            id: id,
+            bookings: {
+                rooms: {
+                    location_id: locationID,
+                }
+            }
+        },
+        include: includeAll,
+        take: limit,
+        skip: offset,
+    })
+        .then(mapCustomID);
 }
 
 export async function deletePayment(id: number) {
-  return prisma.payment.delete({
-    where: {
-      id
-    }
-  });
+    return prisma.payment.delete({
+        where: {
+            id
+        }
+    });
 }
 
 export async function getPaymentStatus() {
-  return prisma.paymentStatus.findMany({
-    orderBy: {
-      status: 'asc'
-    }
-  });
+    return prisma.paymentStatus.findMany({
+        orderBy: {
+            status: 'asc'
+        }
+    });
 }
 
 export async function updatePaymentByID(id: number, data: OmitIDTypeAndTimestamp<Payment>) {
-  return prisma.payment.update({
-    where: {
-      id
-    },
-    data: {
-      ...data
-    },
-    include: includeAll
-  })
-    .then(nb => ({
-        ...nb,
-        custom_id: `#-${nb.bookings.id}`
-      })
-    );
+    return prisma.payment.update({
+        where: {
+            id
+        },
+        data: {
+            ...data
+        },
+        include: includeAll
+    })
+        .then(nb => ({
+                ...nb,
+                bookings: {
+                    ...nb.bookings,
+                    custom_id: `#-${nb.bookings.id}`
+                },
+            })
+        );
 }
 
 export async function createPayment(data: OmitIDTypeAndTimestamp<Payment>, trx?: Prisma.TransactionClient) {
-  const db = trx ?? prisma;
+    const db = trx ?? prisma;
 
-  return db.payment.create({
-    data: {
-      ...data,
-      id: undefined
-    }
-  });
+    return db.payment.create({
+        data: {
+            ...data,
+            id: undefined
+        },
+        include: includeAll
+    })
+        .then(nb => ({
+                ...nb,
+                bookings: {
+                    ...nb.bookings,
+                    custom_id: `#-${nb.bookings.id}`
+                },
+            })
+        );
 }
