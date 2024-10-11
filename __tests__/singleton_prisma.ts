611e3e451b844f48a36f7219bce4f2a49dd97e64
Raymond Salim <rsalim@domaintools.com>
2024-10-11 14:25:23 +0800

    fix: fix failing UT

--- __tests__/singleton_prisma.ts
+++ __tests__/singleton_prisma.ts
@@ -2,13 +2,17 @@ import {PrismaClient} from '@prisma/client';
 import {DeepMockProxy, mockDeep, mockReset} from 'jest-mock-extended';
 
 import prisma from '../src/app/_lib/primsa';
-import {beforeEach} from "@jest/globals";
+import {beforeEach, describe, test} from "@jest/globals";
 
 // @ts-expect-error
 jest.mock('../src/app/_lib/primsa', () => ({
   __esModule: true,
   default: mockDeep<PrismaClient>(),
 }));
+
+describe("", () => {
+  test("", () => {});
+});
 
 beforeEach(() => {
   mockReset(prismaMock);
