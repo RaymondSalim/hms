f40fcb0858dba82d0cdde9b7bc89f9509b1eba0c
Raymond Salim <rsalim@domaintools.com>
2024-10-18 19:39:40 +0800

    feat(util): add random password generator

--- src/app/_lib/util.ts
+++ src/app/_lib/util.ts
@@ -89,3 +89,20 @@ export async function fileToBase64(file: File): Promise<string> {
     reader.readAsDataURL(file);
   });
 }
+
+export function generateRandomPassword(length: number) {
+  const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lower = "abcdefghijklmnopqrstuvwxyz";
+  const numbers = "0123456789";
+  const special = "!@#$%^&*()-_=+[]{}|;:,.<>?/~`";
+  const allChars = upper + lower + numbers + special;
+
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * allChars.length);
+    password += allChars[randomIndex];
+  }
+
+  return password;
+}
