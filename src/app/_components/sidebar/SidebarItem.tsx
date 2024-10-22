ea6028daa88fe3bb196107dff9afbb031be46cf5
Raymond Salim <rsalim@domaintools.com>
2024-10-22 20:43:26 +0800

    feat(sidebar): make items with child not redirect when clicked

--- src/app/_components/sidebar/SidebarItem.tsx
+++ src/app/_components/sidebar/SidebarItem.tsx
@@ -10,119 +10,129 @@ import React, {useRef, useState} from "react";
 import {signOut} from "next-auth/react";
 
 interface SidebarItemProps {
-  name: string;
-  path: string;
-  icon: React.ReactNode;
-  children?: Array<{ name: string; path: string }>;
+    name: string;
+    path: string;
+    icon: React.ReactNode;
+    children?: Array<{ name: string; path: string }>;
 }
 
-export function SidebarItem({ name, path, icon, children }: SidebarItemProps) {
-  const [isOpen, setIsOpen] = useState(false);
+export function SidebarItem({name, path, icon, children}: SidebarItemProps) {
+    const [isOpen, setIsOpen] = useState(false);
 
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
 
-  return (
-    <li className={`${styles.menuItem}`}>
-      <div className={`group/itemHeader ${styles.itemHeader}`}>
-        <Link href={path} className={`${styles.item} group-hover/itemHeader:text-white`}>
-          <span className={styles.icon}>{icon}</span>
-          <span className={styles.text}>{name}</span>
-        </Link>
-        {children && (
-          <span className={`${styles.dropdownIcon} ${isOpen ? styles.open : ''}`} onClick={toggleDropdown}>
-            <FaChevronDown />
+    return (
+        <li className={`${styles.menuItem}`}>
+            <div className={`group/itemHeader ${styles.itemHeader}`}>
+                <Link
+                    onClick={(e) => {
+                        if (children) {
+                            e.preventDefault();
+                            toggleDropdown();
+                        }
+                    }}
+                    href={path}
+                    className={`${styles.item} group-hover/itemHeader:text-white`}
+                >
+                    <span className={styles.icon}>{icon}</span>
+                    <span className={styles.text}>{name}</span>
+                </Link>
+                {children && (
+                    <span className={`${styles.dropdownIcon} ${isOpen ? styles.open : ''}`} onClick={toggleDropdown}>
+            <FaChevronDown/>
           </span>
-        )}
-      </div>
-      {children && (
-        <motion.ul initial={{ height: 0 }} animate={isOpen ? { height: 'auto' } : undefined} className={styles.dropdownMenu}>
-          {children.map((child, index) => (
-            <li key={index} className={`group/childMenuItem ${styles.menuItem}`}>
-              <Link href={child.path} className={`${styles.item} group-hover/childMenuItem:text-white`}>
-                <span className={styles.text}>{child.name}</span>
-              </Link>
-            </li>
-          ))}
-        </motion.ul>
-      )}
-    </li>
-  );
+                )}
+            </div>
+            {children && (
+                <motion.ul initial={{height: 0}} animate={isOpen ? {height: 'auto'} : undefined}
+                           className={styles.dropdownMenu}>
+                    {children.map((child, index) => (
+                        <li key={index} className={`group/childMenuItem ${styles.menuItem}`}>
+                            <Link href={child.path} className={`${styles.item} group-hover/childMenuItem:text-white`}>
+                                <span className={styles.text}>{child.name}</span>
+                            </Link>
+                        </li>
+                    ))}
+                </motion.ul>
+            )}
+        </li>
+    );
 }
 
 export function InteractiveUserDropdown({user}: { user: SiteUser }) {
-  "use client";
-  const [isUserDropdownOpen, setIsUserDropdownOpen] = useState(false);
-  const dropdownRef = useRef<HTMLDivElement>(null);
-  const userAvatarRef = useRef<HTMLDivElement>(null);
+    "use client";
+    const [isUserDropdownOpen, setIsUserDropdownOpen] = useState(false);
+    const dropdownRef = useRef<HTMLDivElement>(null);
+    const userAvatarRef = useRef<HTMLDivElement>(null);
 
-  const handleBlur = (event: React.FocusEvent<HTMLDivElement>) => {
-    if (!event.relatedTarget || !dropdownRef.current?.contains(event.relatedTarget as Node)) {
-      if (event.relatedTarget !== userAvatarRef.current) {
-        setIsUserDropdownOpen(false);
-      }
-    }
-  };
+    const handleBlur = (event: React.FocusEvent<HTMLDivElement>) => {
+        if (!event.relatedTarget || !dropdownRef.current?.contains(event.relatedTarget as Node)) {
+            if (event.relatedTarget !== userAvatarRef.current) {
+                setIsUserDropdownOpen(false);
+            }
+        }
+    };
 
-  const toggleUserDropdown = () => {
-    setIsUserDropdownOpen((prevState) => {
-      if (!prevState) {
-        dropdownRef.current?.focus();
-      }
-      return !prevState;
-    });
-  };
+    const toggleUserDropdown = () => {
+        setIsUserDropdownOpen((prevState) => {
+            if (!prevState) {
+                dropdownRef.current?.focus();
+            }
+            return !prevState;
+        });
+    };
 
-  return (
-    <>
-      <div
-        className={`group/footerUser ${styles.footerUser}`}
-        onClick={toggleUserDropdown}
-        ref={userAvatarRef}
-        tabIndex={0}
-      >
-        <div className={`${styles.userAvatar} group-hover/footerUser:border-opacity-100`}>
-          {/*TODO! Image*/}
-          <span>US</span>
-        </div>
-        <FaChevronDown className={`${styles.userDropdownIcon} group-hover/footerUser:opacity-100`} />
-      </div>
-      <motion.div
-        initial={{ scale: 0, transformOrigin: 'left bottom' }}
-        animate={isUserDropdownOpen ? { scale: 1 } : undefined}
-        className={styles.footerUserDropdown}
-        ref={dropdownRef}
-        tabIndex={-1}
-        onBlur={handleBlur}
-      >
-        <div className={styles.userInfo}>
-          <span>{user?.name}</span>
-          <span>{user?.email}</span>
-        </div>
-        <ul className={styles.dropdownList}>
-          <li>
-            <Link href="/whats-new">What&apos;s new?</Link>
-          </li>
-          <li>
-            <Link href="/profile">Your profile</Link>
-          </li>
-          <li>
-            <Link href="/help-center">Help center</Link>
-          </li>
-          <li>
-            <Link href="/resources-guides">Resources & guides</Link>
-          </li>
-          <li>
-            <Link href="/sign-out">Sign out</Link>
-          </li>
-        </ul>
-      </motion.div>
-      <div className={styles.footerOptions}>
-        <div title="Logout" className={styles.logoutBtn} onClick={() => signOut({callbackUrl: "/login"})}>
-          <CiLogout />
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div
+                className={`group/footerUser ${styles.footerUser}`}
+                onClick={toggleUserDropdown}
+                ref={userAvatarRef}
+                tabIndex={0}
+            >
+                <div className={`${styles.userAvatar} group-hover/footerUser:border-opacity-100`}>
+                    {/*TODO! Image*/}
+                    <span>US</span>
+                </div>
+                <FaChevronDown className={`${styles.userDropdownIcon} group-hover/footerUser:opacity-100`}/>
+            </div>
+            <motion.div
+                initial={{scale: 0, transformOrigin: 'left bottom'}}
+                animate={isUserDropdownOpen ? {scale: 1} : undefined}
+                className={styles.footerUserDropdown}
+                ref={dropdownRef}
+                tabIndex={-1}
+                onBlur={handleBlur}
+            >
+                <div className={styles.userInfo}>
+                    <span>{user?.name}</span>
+                    <span>{user?.email}</span>
+                </div>
+                <ul className={styles.dropdownList}>
+                    <li>
+                        <Link href="/whats-new">What&apos;s new?</Link>
+                    </li>
+                    <li>
+                        <Link href="/profile">Your profile</Link>
+                    </li>
+                    <li>
+                        <Link href="/help-center">Help center</Link>
+                    </li>
+                    <li>
+                        <Link href="/resources-guides">Resources & guides</Link>
+                    </li>
+                    <li>
+                        <Link href="/sign-out">Sign out</Link>
+                    </li>
+                </ul>
+            </motion.div>
+            <div className={styles.footerOptions}>
+                <div title="Logout" className={styles.logoutBtn} onClick={() => signOut({callbackUrl: "/login"})}>
+                    <CiLogout/>
+                </div>
+            </div>
+        </>
+    );
 }
