33ba24ce02a947918cc15028e2eeef2bfcae4035
Raymond Salim <rsalim@domaintools.com>
2024-11-20 16:22:01 +0800

    chore: add TODO comment

--- src/app/(internal)/(dashboard_layout)/expenses/expenses-action.ts
+++ src/app/(internal)/(dashboard_layout)/expenses/expenses-action.ts
@@ -5,103 +5,105 @@ import {createExpense, deleteExpense, updateExpenseByID} from "@/app/_db/expense
 import {number, object} from "zod";
 
 export type ExpenseActionsType<T = OmitIDTypeAndTimestamp<Expense>> = {
-  success?: string | Expense;
-  failure?: string;
-  errors?: Partial<Expense>;
+    success?: string | Expense;
+    failure?: string;
+    errors?: Partial<Expense>;
 };
 
 export async function createExpenseAction(prevState: ExpenseActionsType, formData: FormData): Promise<ExpenseActionsType> {
-  const parsedData = expenseObject.safeParse({
-    amount: parseFloat(formData.get("amount") as string),
-    description: formData.get("description"),
-    date: formData.get("date"),
-    category_id: formData.get("category_id") ? parseInt(formData.get("category_id") as string) : null,
-    location_id: formData.get("location_id") ? parseInt(formData.get("category_id") as string) : null
-  });
-
-  if (!parsedData.success) {
-    return {
-      errors: Object.fromEntries(parsedData.error.errors.entries()),
-    };
-  }
-
-  try {
-    let res = await createExpense({
-      ...parsedData.data,
-      category_id: parsedData.data.category_id,
-      date: new Date(parsedData.data.date),
-      amount: new Prisma.Decimal(parsedData.data.amount)
+    // TODO! Ensure date is in UTC
+    const parsedData = expenseObject.safeParse({
+        amount: parseFloat(formData.get("amount") as string),
+        description: formData.get("description"),
+        date: formData.get("date"),
+        category_id: formData.get("category_id") ? parseInt(formData.get("category_id") as string) : null,
+        location_id: formData.get("location_id") ? parseInt(formData.get("category_id") as string) : null
     });
 
-    return {
-      success: res,
-    };
-  } catch (error) {
-    console.error(error);
+    if (!parsedData.success) {
+        return {
+            errors: Object.fromEntries(parsedData.error.errors.entries()),
+        };
+    }
 
-    return {
-      failure: "Error creating expense",
-    };
-  }
+    try {
+        let res = await createExpense({
+            ...parsedData.data,
+            category_id: parsedData.data.category_id,
+            date: new Date(parsedData.data.date),
+            amount: new Prisma.Decimal(parsedData.data.amount)
+        });
+
+        return {
+            success: res,
+        };
+    } catch (error) {
+        console.error(error);
+
+        return {
+            failure: "Error creating expense",
+        };
+    }
 }
 
 export async function updateExpenseAction(prevState: ExpenseActionsType, formData: FormData): Promise<ExpenseActionsType> {
-  const parsedData = expenseObjectWithID.safeParse({
-    id: parseInt(formData.get("id") as string),
-    amount: parseFloat(formData.get("amount") as string),
-    description: formData.get("description"),
-    date: formData.get("date"),
-    category_id: formData.get("category_id") ? parseInt(formData.get("category_id") as string) : undefined,
-  });
-
-  if (!parsedData.success) {
-    return {
-      errors: Object.fromEntries(parsedData.error.errors.entries()),
-    };
-  }
-
-  try {
-    let res = await updateExpenseByID(parsedData.data.id, {
-      ...parsedData.data,
-      category_id: parsedData.data.category_id ?? null,
-      date: new Date(parsedData.data.date),
-      amount: new Prisma.Decimal(parsedData.data.amount)
+    // TODO! Ensure date is in UTC
+    const parsedData = expenseObjectWithID.safeParse({
+        id: parseInt(formData.get("id") as string),
+        amount: parseFloat(formData.get("amount") as string),
+        description: formData.get("description"),
+        date: formData.get("date"),
+        category_id: formData.get("category_id") ? parseInt(formData.get("category_id") as string) : undefined,
     });
 
-    return {
-      success: res,
-    };
-  } catch (error) {
-    console.error(error);
+    if (!parsedData.success) {
+        return {
+            errors: Object.fromEntries(parsedData.error.errors.entries()),
+        };
+    }
 
-    return {
-      failure: "Error updating expense",
-    };
-  }
+    try {
+        let res = await updateExpenseByID(parsedData.data.id, {
+            ...parsedData.data,
+            category_id: parsedData.data.category_id ?? null,
+            date: new Date(parsedData.data.date),
+            amount: new Prisma.Decimal(parsedData.data.amount)
+        });
+
+        return {
+            success: res,
+        };
+    } catch (error) {
+        console.error(error);
+
+        return {
+            failure: "Error updating expense",
+        };
+    }
 }
 
 export async function deleteExpenseAction(prevState: ExpenseActionsType<Pick<Expense, "id">>, formData: FormData): Promise<ExpenseActionsType<Pick<Expense, "id">>> {
-  const parsedData = object({ id: number().positive() }).safeParse({
-    id: parseInt(formData.get("id") as string),
-  });
+    const parsedData = object({id: number().positive()}).safeParse({
+        id: parseInt(formData.get("id") as string),
+    });
 
-  if (!parsedData.success) {
-    return {
-      errors: Object.fromEntries(parsedData.error.errors.entries()),
-    };
-  }
+    if (!parsedData.success) {
+        return {
+            errors: Object.fromEntries(parsedData.error.errors.entries()),
+        };
+    }
 
-  try {
-    let res = await deleteExpense(parsedData.data.id);
+    try {
+        let res = await deleteExpense(parsedData.data.id);
 
-    return {
-      success: res,
-    };
-  } catch (error) {
-    console.error(error);
+        return {
+            success: res,
+        };
+    } catch (error) {
+        console.error(error);
 
-    return {
-      failure: "Error deleting expense",
-    };
-  }
+        return {
+            failure: "Error deleting expense",
+        };
+    }
 }
