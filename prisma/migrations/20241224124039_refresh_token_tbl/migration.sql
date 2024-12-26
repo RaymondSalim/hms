0a770ffc60a0808f189aff8041fdbc9e3e2e8f52
Raymond Salim <rsalim@domaintools.com>
2024-12-27 00:27:41 +0700

    feat: add missing migration files

---
+++ prisma/migrations/20241224124039_refresh_token_tbl/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "RefreshToken" (
+    "id" SERIAL NOT NULL,
+    "token" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "blacklisted" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "RefreshToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "RefreshToken_token_key" ON "RefreshToken"("token");
+
+-- AddForeignKey
+ALTER TABLE "RefreshToken" ADD CONSTRAINT "RefreshToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "siteusers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
